b37f02a8df476fd462577b8e452e5eb9ee898eac
Francesly Hebe <f.francesly@gmail.com>
2023-01-10 13:42:40 -0300

    Add project files.

---
+++ Tarefas/Models/EnumStatusTarefa.cs
@@ -0,0 +1,8 @@
+﻿namespace Tarefas.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
---
+++ Tarefas/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Tarefas.Context;
+
+namespace Tarefas
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<OrganizadorContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
